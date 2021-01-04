97e822193f23b4fa2f8ce5e8ba8982680b21e6ba
kwansai724 <kwansai.taka724@gmail.com>
2021-01-04 14:54:41 +0000

    11.2

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -29,6 +29,11 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
-    debugger
+  end
+  
+  private
+  
+  def attendances_params
+    params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
   end
 end
